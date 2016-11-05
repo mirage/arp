bc57f7001eff1baddac08cbc56815f501c789b42
Hannes Mehnert <hannes@mehnert.org>
2016-11-05 00:35:08 +0000

    coverage support via bisect_ppx in pkg/pkg.ml
    If `--with-coverage true`, pass `-pkg bisect_ppx` to ocamlbuild
    
    @dbuenzli wanted to be tagged (see topkg#72)

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,16 +4,28 @@
 open Topkg
 
 let mirage = Conf.with_pkg ~default:false "mirage"
+let coverage = Conf.with_pkg ~default:false "coverage"
 
 let distrib =
   let exclude_paths () = Pkg.exclude_paths () >>| fun ps -> "bench" :: ps in
   Pkg.distrib ~exclude_paths ()
 
+let cmd c os files =
+  let build =
+    if Conf.value c coverage then
+      let coverage_arg = Cmd.(v "-pkg" % "bisect_ppx") in
+      let coverage_cmd c os = Cmd.(Pkg.build_cmd c os %% coverage_arg) in
+      coverage_cmd
+    else
+      Pkg.build_cmd
+  in
+  OS.Cmd.run @@ Cmd.(build c os %% of_list files)
+
 let () =
   let opams =
     [ Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["ppx_tools"]) ]
   in
-  Pkg.describe ~opams "arp" @@ fun c ->
+  Pkg.describe ~build:(Pkg.build ~cmd ()) ~opams "arp" @@ fun c ->
   let mirage = Conf.value c mirage in
   Ok [
     Pkg.mllib "src/arp.mllib";
